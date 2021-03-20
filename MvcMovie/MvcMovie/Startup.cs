5898d21f59eaefc4fbc349f64cdf8831f710160f
Nik Trush <nick_trush@mail.ru>
2021-03-20 13:23:24 +0300

    Deleted Hello and Home realization

--- MvcMovie/MvcMovie/Startup.cs
+++ MvcMovie/MvcMovie/Startup.cs
@@ -40,7 +40,7 @@ namespace MvcMovie
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Movies/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -55,7 +55,7 @@ namespace MvcMovie
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
